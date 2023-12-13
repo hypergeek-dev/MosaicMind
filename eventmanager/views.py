f556f75d9ab1d09935d40096dd37c06898c22a0e
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-13 03:01:38 +0100

    Creating basic views for rendering

--- eventmanager/views.py
+++ eventmanager/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about_us(request):
+    return render(request, 'aboutus.html')
+
+def add_meeting(request):
+    return render(request, 'addmeeting.html')
+
+def edit_meeting(request):
+    return render(request, 'editmeeting.html')
+
+def meeting_finder(request):
+    return render(request, 'meetingfinder.html')
+
+def volunteer(request):
+    return render(request, 'volunteer.html')
