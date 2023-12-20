bc85eda73ef25cfb259877c8626ea5065b7bacdb
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-20 23:55:55 +0100

    Update routes and import statements in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BaseLayout from './BaseLayout';
 import Home from './templates/home';
 import AboutUs from './templates/aboutus';
-import AddEditMeeting from './templates/add_edit_meeting';
+import AddMeeting from './templates/add_meeting';
+import EditMeeting from './templates/edit_meeting';
 import MeetingFinder from './templates/meetingfinder';
 import MeetingList from './templates/meetinglist';
 import Volunteer from './templates/volunteer';
@@ -15,8 +17,9 @@ function App() {
                 <Routes>
                     <Route path="/templates/home" element={<Home />} />
                     <Route path="/templates/about-us" element={<AboutUs />} />
-                    <Route path="/templates/add-edit-meeting" element={<AddEditMeeting />} />
-                    <Route path="/templates/meetingfinder" element={<MeetingFinder />} />
+                    <Route path="/templates/add_meeting" element={<AddMeeting/>} />
+                    <Route path="/templates/edit_meeting" element={<EditMeeting/>} />
+                    <Route path="/templates/meetingfinder" element={<MeetingFinder/>} />
                     <Route path="/templates/meetinglist" element={<MeetingList />} />
                     <Route path="/templates/volunteer" element={<Volunteer />} />
                 </Routes>
