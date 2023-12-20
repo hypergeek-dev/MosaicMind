38b045c8affd60fcc5d712bbd619356272230470
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-20 23:57:40 +0100

    Add admin page template

---
+++ frontend/src/templates/admin_page.js
@@ -0,0 +1,36 @@
+<div class="container mt-3">
+    <div class="row">
+      <!-- Waiting for Approval Section -->
+      <div class="col-md-8">
+        <h2>Waiting for approval</h2>
+        <div id="meetingsApprovalList">
+          <!-- Virtual Meeting Entry -->
+          <div class="card mb-2">
+            <div class="card-body">
+              <h5 class="card-title">Virtual meeting</h5>
+              <p class="card-text">Time: <span class="meeting-time">[Time]</span></p>
+              <p class="card-text">Online Link: <a href="#" class="meeting-link">[Link]</a></p>
+              <p class="card-text">Description: <span class="meeting-description">[Description]</span></p>
+            </div>
+          </div>
+          <!-- Repeat for each virtual meeting entry -->
+        </div>
+      </div>
+  
+      <!-- User Administration Section -->
+      <div class="col-md-4">
+        <h2>User administration</h2>
+        <div id="userAdminList" class="list-group">
+          <!-- User Entry -->
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="userJohn">
+            <label class="form-check-label" for="userJohn">
+              John
+            </label>
+          </div>
+          <!-- Repeat for each user -->
+        </div>
+      </div>
+    </div>
+  </div>
+  
