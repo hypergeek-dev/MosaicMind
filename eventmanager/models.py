c89663bbcb16efb7fe9f60b45ad656428de886dd
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-20 23:55:26 +0100

    Add User and Meeting models to models.py

--- eventmanager/models.py
+++ eventmanager/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import uuid
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class User(models.Model):
+    f_name = models.CharField(max_length=100)
+    l_name = models.CharField(max_length=100)
+    alias = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_number = PhoneNumberField(blank=True)
+
+    def __str__(self):
+        return self.name# your fields here
+from django.db import models
+from django.conf import settings
+import uuid
+
+class Meeting(models.Model):
+    meeting_id = models.CharField(max_length=100, default=uuid.uuid4, unique=True)
+    name = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    description = models.TextField()
+    online_meeting_url = models.URLField()
+
+    # ForeignKey relationship to the User model
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='meetings'
+    )
+
+
+    def __str__(self):
+        return self.name
