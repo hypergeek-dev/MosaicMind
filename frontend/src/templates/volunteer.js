7d7be4406bc41a69cde332eb59bc5373de83aec9
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-21 13:58:55 +0100

    Add Volunteer component

---
+++ frontend/src/templates/volunteer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Volunteer() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Placehoder for Volunteerpage</p>
+           
+        </div>
+    );
+}
+
+export default Volunteer;
