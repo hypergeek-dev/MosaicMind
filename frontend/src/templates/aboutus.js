bf600c37e8ad55830a763a531346faa0a2f3bbb7
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-21 13:58:36 +0100

    Add About Us page component

---
+++ frontend/src/templates/aboutus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Placeholder for About us page</p>
+        </div>
+    );
+}
+
+export default AboutUs;
