d23ccbe1e898dd2697f05066bb2f416c93ac059c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-21 09:53:48 +0100

    Refactor User and Meeting models in models.py

--- eventmanager/models.py
+++ eventmanager/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 import uuid
-from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 class User(models.Model):
@@ -11,17 +10,62 @@ class User(models.Model):
     phone_number = PhoneNumberField(blank=True)
 
     def __str__(self):
-        return self.name# your fields here
-from django.db import models
-from django.conf import settings
-import uuid
+        return f"{self.f_name} {self.l_name}"  
 
 class Meeting(models.Model):
     meeting_id = models.CharField(max_length=100, default=uuid.uuid4, unique=True)
-    name = models.CharField(max_length=100)
-    area = models.CharField(max_length=100)
-    description = models.TextField()
-    online_meeting_url = models.URLField()
+    name = models.CharField(max_length=100, default="Mosaic Minds Meeting")
+    meeting_time = models.TimeField(default=datetime.time(12, 0))
+        
+    WEEKDAY = (
+        ('SUN', 'Sunday'),
+        ('MON', 'Monday'),
+        ('TUE', 'Tuesday'),
+        ('WED', 'Wednesday'),
+        ('THU', 'Thursday'),
+        ('FRI', 'Friday'),
+        ('SAT', 'Saturday'),
+    )
+    
+    AREA_CHOICES = (
+        ('CI', 'Channel Islands Area'),
+        ('CTV', 'Chiltern & Thames Valley Area'),
+        ('CA', 'Cornwall Area'),
+        ('DA', 'Devon Area'),
+        ('DO', 'Dorset Area'),
+        ('EMA', 'East Midlands Area'),
+        ('ES', 'Essex Area'),
+        ('FC', 'Free Counties Area'),
+        ('GM', 'Greater Manchester Area'),
+        ('HA', 'Hampshire Area'),
+        ('KE', 'Kent Area'),
+        ('L-EC', 'Lancashire - East & Central Area'),
+        ('L-NE', 'London - North East Area'),
+        ('L-NW', 'London - North West Area'),
+        ('L-SE', 'London - South East Area'),
+        ('L-SW', 'London - South West Area'),
+        ('MEA', 'Merseyside Area'),
+        ('NS', 'Norfolk & Suffolk Area'),
+        ('NEE', 'North East England Area'),
+        ('NW', 'NW England & N Wales Area'),
+        ('S-EC', 'Scotland - East Coast Area'),
+        ('S-EL', 'Scotland - Edinburgh & Lothians Area'),
+        ('S-GW', 'Scotland - Glasgow & West Coast Area'),
+        ('S-WL', 'Scotland - West Lothian Area'),
+        ('SWA', 'South Wales Area'),
+        ('SU', 'Surrey Area'),
+        ('SX', 'Sussex Area'),
+        ('HIG', 'The Highlands & Islands Groups'),
+        ('TS', 'The Shires Area'),
+        ('UK-FAR', 'UK Farsi Groups Area'),
+        ('WC', 'West Country Area'),
+        ('WM', 'West Midlands Area'),
+        ('WY', 'West Yorkshire'),
+        ('YH', 'Yorkshire & Humberside Area'),
+    )
+    area = models.CharField(max_length=20, choices=AREA_CHOICES, default='CI')
+    description = models.TextField(default="Standard meeting description.")
+    online_meeting_url = models.URLField(default="www.google.com")
 
     # ForeignKey relationship to the User model
     added_by = models.ForeignKey(
@@ -29,7 +73,6 @@ class Meeting(models.Model):
         on_delete=models.CASCADE,
         related_name='meetings'
     )
-
 
     def __str__(self):
         return self.name
