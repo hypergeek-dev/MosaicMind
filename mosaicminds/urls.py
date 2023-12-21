c2294b2586bff5a2891b4bdc73a66621430373da
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-21 17:58:40 +0100

    Add MeetingListAll view to URL patterns

--- mosaicminds/urls.py
+++ mosaicminds/urls.py
@@ -2,11 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from eventmanager import views
+from django.urls import path
+from eventmanager.views import MeetingListAll
 
 urlpatterns = [
     path('admin/', admin.site.urls),path('', views.index, name='index'),
     path('about/', views.about_us, name='about_us'),
     path('add_meeting/', views.add_meeting, name='add_meeting'),
+    path('meeting_list_all/', MeetingListAll, name='meeting_list_all'),
     path('edit_meeting/', views.edit_meeting, name='edit_meeting'),
     path('meeting_finder/', views.meeting_finder, name='meeting_finder'),
     path('volunteer/', views.volunteer, name='volunteer'),
