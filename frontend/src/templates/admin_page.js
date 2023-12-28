0df2882ff7b6f344c226a8530c07b7506ee6e441
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-28 07:59:29 +0100

    Remove commented out code and fix formatting in admin_page.js

--- frontend/src/templates/admin_page.js
+++ frontend/src/templates/admin_page.js
@@ -1,10 +1,9 @@
 <div class="container mt-3">
     <div class="row">
-      <!-- Waiting for Approval Section -->
+     
       <div class="col-md-8">
         <h2>Waiting for approval</h2>
         <div id="meetingsApprovalList">
-          <!-- Virtual Meeting Entry -->
           <div class="card mb-2">
             <div class="card-body">
               <h5 class="card-title">Virtual meeting</h5>
@@ -13,24 +12,22 @@
               <p class="card-text">Description: <span class="meeting-description">[Description]</span></p>
             </div>
           </div>
-          <!-- Repeat for each virtual meeting entry -->
         </div>
       </div>
   
-      <!-- User Administration Section -->
       <div class="col-md-4">
         <h2>User administration</h2>
         <div id="userAdminList" class="list-group">
-          <!-- User Entry -->
+       
           <div class="form-check">
             <input class="form-check-input" type="checkbox" value="" id="userJohn">
             <label class="form-check-label" for="userJohn">
               John
             </label>
-          </div>
-          <!-- Repeat for each user -->
+          </input>
+       
         </div>
       </div>
     </div>
   </div>
-  
+  </div>
