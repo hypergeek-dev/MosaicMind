047a8cf1657b3956229e61e8e0f7b16ee00a93b9
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-28 08:00:45 +0100

    Update MeetingSerializer to use added_by_username instead of added_by_alias

--- eventmanager/serializer.py
+++ eventmanager/serializer.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import Meeting
+from django.contrib.auth.models import User
 
 class MeetingSerializer(serializers.ModelSerializer):
-    added_by_alias = serializers.SerializerMethodField()
+    added_by_username = serializers.SerializerMethodField()
 
     class Meta:
         model = Meeting
-        fields = ['name', 'area', 'description', 'online_meeting_url', 'added_by_alias']
+        fields = ['name', 'area', 'description', 'online_meeting_url', 'added_by_username']
 
-    def get_added_by_alias(self, obj):
-        return obj.added_by.alias if obj.added_by else None
+    def get_added_by_username(self, obj):
+        return obj.added_by.username if obj.added_by else None
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']  
