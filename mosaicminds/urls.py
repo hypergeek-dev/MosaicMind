81cce4ca0475172c4832bee03d38c66337d770cd
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-12-28 10:09:23 +0100

    Refactor URLs in mosaicminds app

--- mosaicminds/urls.py
+++ mosaicminds/urls.py
@@ -1,16 +1,14 @@
-
 from django.contrib import admin
 from django.urls import path
-from eventmanager import views
-from django.urls import path
-from eventmanager.views import MeetingListAll
+from eventmanager.views import index, about_us, add_meeting, MeetingListAll, edit_meeting, meeting_finder, volunteer
 
 urlpatterns = [
-    path('admin/', admin.site.urls),path('', views.index, name='index'),
-    path('about/', views.about_us, name='about_us'),
-    path('add_meeting/', views.add_meeting, name='add_meeting'),
-    path('meeting_list_all/', MeetingListAll, name='meeting_list_all'),
-    path('edit_meeting/', views.edit_meeting, name='edit_meeting'),
-    path('meeting_finder/', views.meeting_finder, name='meeting_finder'),
-    path('volunteer/', views.volunteer, name='volunteer'),
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('about/', about_us, name='about_us'),
+    path('add_meeting/', add_meeting, name='add_meeting'),
+    path('meeting_list_all/', MeetingListAll, name='meeting_list_all'),  
+    path('edit_meeting/', edit_meeting, name='edit_meeting'),
+    path('meeting_finder/', meeting_finder, name='meeting_finder'),
+    path('volunteer/', volunteer, name='volunteer'),
 ]
