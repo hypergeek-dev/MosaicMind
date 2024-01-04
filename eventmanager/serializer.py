d62911f56b4603ad6ff3c6ff8fcbb2f42579b09c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-04 17:50:57 +0000

    Adding LoginSerializer

--- eventmanager/serializer.py
+++ eventmanager/serializer.py
@@ -25,3 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
