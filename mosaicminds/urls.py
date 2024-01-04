e23978ac315d8eb2f0306e0c933308604d354366
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-04 10:52:10 +0000

    Refactoring urls.py for backend clarity

--- mosaicminds/urls.py
+++ mosaicminds/urls.py
@@ -1,14 +1,21 @@
-from django.contrib import admin
 from django.urls import path
-from eventmanager.views import index, about_us, add_meeting, MeetingListAll, edit_meeting, meeting_finder, volunteer
+from eventmanager import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('about/', about_us, name='about_us'),
-    path('add_meeting/', add_meeting, name='add_meeting'),
-    path('meeting_list_all/', MeetingListAll, name='meeting_list_all'),
-    path('edit_meeting/', edit_meeting, name='edit_meeting'),
-    path('meeting_finder/', meeting_finder, name='meeting_finder'),
-    path('volunteer/', volunteer, name='volunteer'),
+    
+    path('index/', views.index, name='api_index'),
+    path('about-us/', views.about_us, name='api_about_us'),
+    path('add-meeting/', views.add_meeting, name='api_add_meeting'),
+    path('meeting-finder/', views.meeting_finder, name='api_meeting_finder'),
+    path('volunteer/', views.volunteer, name='api_volunteer'),
+    path('edit-meeting/', views.edit_meeting, name='api_edit_meeting'),
+    path('meeting-list-all/', views.MeetingListAll, name='api_meeting_list_all'),
+
+  
+    path('meetings/', views.MeetingList.as_view(), name='api_meetings'),
+    path('users/', views.UserList.as_view(), name='api_users'),
+
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
