dbe5e79cbc362376e4b1a539b52a60c1cb40ffa1
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-06 10:11:09 +0000

    Adding repath import to urls.py

--- mosaicminds/urls.py
+++ mosaicminds/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from eventmanager import views
+from django.urls import path, re_path
+from django.views.generic import TemplateVie
 
 urlpatterns = [
     path('index/', views.index, name='index'),
