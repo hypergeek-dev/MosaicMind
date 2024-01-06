8db7a22d48170b480f511e5f67cec4468c7f8a04
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-06 12:00:27 +0000

    Changing src path for staticfiles

--- mosaicminds/settings.py
+++ mosaicminds/settings.py
@@ -94,8 +94,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),  
 ]
