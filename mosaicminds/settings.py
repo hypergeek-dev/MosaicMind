d179639cfe5b4d6f190ff8ece9fb90d50117ee8b
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-06 13:02:40 +0000

    Enhanced security settings and disabled debug mode for production

--- mosaicminds/settings.py
+++ mosaicminds/settings.py
@@ -94,15 +94,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static/'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build'),  
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -117,5 +114,10 @@ LOGIN_URL = 'login/'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-DEBUG = True
-
+# Security settings
+SECURE_HSTS_SECONDS = 3600  # or more
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
