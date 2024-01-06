e13b1f4abd7d9f517d6b440538096220e2d7df6c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2024-01-06 12:18:06 +0000

    Collecting static files

--- mosaicminds/settings.py
+++ mosaicminds/settings.py
@@ -94,8 +94,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/build/static/'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
